cf14c79435c34d482620cf8b38595f9ce4b45bdf
cuifengchuan <cuinidaye@gmail.com>
2016-04-21 20:31:29 +0800

    git init

--- Reading/ViewController.swift
+++ Reading/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Title"
+        self.view.backgroundColor = UIColor.lightGrayColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
