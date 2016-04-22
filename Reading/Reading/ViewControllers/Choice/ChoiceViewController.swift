b003da5bd764736d262f9764bd3fa801fb3c1138
cuifengchuan <cuinidaye@gmail.com>
2016-04-22 16:32:10 +0800

    建立ViewController

---
+++ Reading/Reading/ViewControllers/Choice/ChoiceViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ChoiceViewController.swift
+//  Reading
+//
+//  Created by cuifengchuan on 16/4/22.
+//  Copyright © 2016年 cuifengchuan. All rights reserved.
+//
+
+import UIKit
+
+class ChoiceViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "精选"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Reading/Reading/ViewControllers/UserCenter/UserCenterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UserCenterViewController.swift
+//  Reading
+//
+//  Created by cuifengchuan on 16/4/22.
+//  Copyright © 2016年 cuifengchuan. All rights reserved.
+//
+
+import UIKit
+
+class UserCenterViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "我的"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
